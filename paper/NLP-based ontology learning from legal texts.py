866e60e14fa8612c481bfa36a7a9b26b9ea95677
mhannani <mohamed_hanani2019@gmail.com>
2022-01-16 00:39:21 +0100

    dataset class to get data ready.

---
+++ paper/NLP-based ontology learning from legal texts.py
@@ -0,0 +1,41 @@
+"""
+-----------
+Discussion:
+-----------
+
+Paper:
+------
+NLP-based ontology learning from legal texts. A case study paper
+
+
+
+Ontology
+--------
+Formally-defined vocabulary for a particular domain of interest used to capture
+knowledge about that (restricted) domain of interest.
+The ontology describes the concepts in the domain and the relationship hold
+between those concepts which is necessary for knowledge representation(Field of AI, dedicated
+to representing information about the world in form that a computer system use
+to solve tasks. From wikipedia: https://en.wikipedia.org/wiki/Knowledge_representation_and_reasoning)
+and knowledge exchange.
+
+From : Juliao Braga, Joaquim L R Dias, and Francisco Regateiro. A MACHINE LEARNING ONTOLOGY, 2021
+
+
+
+Legal ontology
+--------------
+        Legal ontology aim to provide a structured representation of legal concepts
+    and their interconnections... They are used to model the domain of knowledge
+    for which a system is developed and the underlying concepts structure.
+
+
+
+Ontology learning
+-----------------
+        Ontology learning is the automatic or semi-automatic creation of ontologies, including
+    extracting the corresponding domain's terms and the relationships between the concepts
+    that these terms represent from a corpus of natural language text, and encoding them
+    with an ontology language for easy retrieval.
+
+"""
---
+++ paper/terms.py
@@ -0,0 +1,19 @@
+"""
+----------------------------
+Useful citations from papers
+----------------------------
+
+
+
+************************************************************************************
+The situation in the legal domain is in line with the trend methodologies proposed
+to automatically extract information from unstructured texts and provide a structured
+organisation of extracted language in such domain and the fact that laws are
+invariably conveyed through natural language.
+----
+From : NLP-based ontology learning from legal texts. A case study.
+************************************************************************************
+
+
+
+"""
